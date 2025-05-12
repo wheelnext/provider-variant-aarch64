478b424b0fb8ba6e01ac273fb2628f5f78bef912
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-05-12 11:42:03 -0400

    Flit + Entry-Point

--- provider_variant_aarch64/__init__.py
+++ provider_variant_aarch64/__init__.py
@@ -1,3 +1,3 @@
-"""A variant provider plugin for AArch64 architecture"""
+"""A plugin to build variant for a specific AArch64 microarchitecture level"""
 
-__version__ = "0.0.1"
+__version__ = "0.0.1.post1"
